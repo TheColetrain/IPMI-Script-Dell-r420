f7bfe291a10d3a054ac11c44a569c37fded17a67
Cole Edwards <coletrainedwards@gmail.com>
2025-11-03 11:54:51 -0600

    Set static fan speed to 40% and clean up script - rought draft
    Updated fan control script to set static fan speed to 40%. Adjusted temperature thresholds and removed unnecessary echo statements.

--- r420ipmifancontroll/static-fan-40percent.sh
+++ r420ipmifancontroll/static-fan-40percent.sh
@@ -11,39 +11,14 @@
 #notes nano /etc/crontab
 #notes  crontab -u root -e
 
-## I did a lot of "echo's" so that i could test my process as I went forward
-## You can omit some echos, if you like. (or just put a # in front)
 
-#Works with 3 variables, mostly Inlet temp (part 2A)
-# but also CPU #1 & #2 temperatures (part 2B)
 
-#### part one - SET VARIABLES only need to set HIGHTEMP to your choice
-# - kicks in manual mode
-#all other temps auto set by cascading
-HIGHTEMP=26
-# you may set #INC = Increment to other than 1 if you want a step-down by more than 1
-INC=1
+#### part one - SET VARIABLES 
+
 # set CPU1 & CPU2 MAX TEMP
-CPU1MAX=50
-CPU2MAX=49
+CPU1MAX=75
+CPU2MAX=75
 
-####part 2A
-echo HIGHTEMP "$HIGHTEMP"
-
-HIGHTEMP2=$((HIGHTEMP - INC))
-echo HIGHTEMP2 "$HIGHTEMP2"
-
-HIGHTEMP3=$((HIGHTEMP2 - INC))
-echo HIGHTEMP3 "$HIGHTEMP3"
-
-HIGHTEMP4=$((HIGHTEMP3 - INC))
-echo HIGHTEMP4 "$HIGHTEMP4"
-
-HIGHTEMP5=$((HIGHTEMP4 - INC))
-echo HIGHTEMP5 "$HIGHTEMP5"
-
-HIGHTEMP6=$((HIGHTEMP5 - INC))
-echo HIGHTEMP6 "$HIGHTEMP6"
 
 
 #### Part 2B
@@ -52,113 +27,79 @@ echo HIGHTEMP6 "$HIGHTEMP6"
 echo CPU1MAX "$CPU1MAX"
 echo CPU2MAX "$CPU2MAX"
 
-#you can change the last# if you want a different CPU threshold for step 2 of CPUTEMP
-#didnt do the math with "INC" like above
 
-CPU1MAX2=$((CPU1MAX - 1))
-CPU2MAX2=$((CPU2MAX - 1))
-CPU1MAX3=$((CPU2MAX - 2))
-CPU2MAX3=$((CPU2MAX - 2))
-
-echo CPU1MAX2 "$CPU1MAX2"
-echo CPU2MAX2 "$CPU2MAX2"
-echo CPU1MAX3 "$CPU1MAX3"
-echo CPU2MAX3 "$CPU2MAX3"
 
 ####Part 3
 
 #this sets your fan speed, you may want to select different Hexadecimal codes
 #based on your needs FS=FAN SPEED. My auto controll is
-#set to 30% min, this works for me.
 
-#Hex conversion link https://www.hexadecimaldictionary.com/hexadecimal/0xf/
+# Static fan speed - change this to test different speeds
+# Start with 40% and adjust up/down based on temps and noise
+# FS40 is 40% and so on.  FS60 is 60% etc
+STATICFAN=$FS40
 
+#Hex conversion https://www.mathsisfun.com/binary-decimal-hexadecimal-converter.html  
+# type 40% as "40" into "decimal" box amd then add prefix of "0x"
+
+FS60=0x3c
+FS58=0x3a
+FS56=0x38
+FS54=0x36
+FS52=0x34
+FS50=0x32
+FS48=0x30
+FS46=0x2e
+FS44=0x2c
+FS42=0x2a
+FS40=0x28
+FS38=0x26
+FS36=0x24
+FS34=0x22
+FS32=0x20
+FS30=0x1e
+FS28=0x1c
+FS26=0x1a
+FS24=0x18
 FS22=0x16
 FS20=0x14
 FS18=0x12
 FS16=0x10
 FS14=0xe
-FS12=oxc
+FS12=0xc
 FS10=0xa
+
 
 #####Part 4
 
 # This gets your temp numbers from IPMITOOL and sets them as a variable.
-#Three temps.   INLET TEMP, CPU1 & CPU2
+#Three temps.   ICPU1 & CPU2
 
-TEMPINLET=`ipmitool sdr type temperature | grep "Temp" | cut -d"|" -f5 | cut -d" " -f2 | sed -n 1p`
+
 TEMPCPU1=`ipmitool sdr type temperature | grep "Temp" | cut -d"|" -f5 | cut -d" " -f2 | sed -n 2p`
 TEMPCPU2=`ipmitool sdr type temperature | grep "Temp" | cut -d"|" -f5 | cut -d" " -f2 | sed -n 3p`
 
 echo " -- current temperature --"
-echo INLET AIR TEMP "$TEMPINLET" C
 echo CPU 1 TEMP     "$TEMPCPU1"  C
 echo CPU 2 TEMP     "$TEMPCPU2"  C
 
 
-# part 5 - FINALE - this pulls it all together. IF temp over X then issue fan controll
+# part 5 - FINALE - this pulls it all together. IF temp over X then issue fan controll to AUTO
 
-if [[ "$TEMPINLET" > "$HIGHTEMP" ||
-      "$TEMPCPU1" > "$CPU1MAX" ||
-      "$TEMPCPU2" > "$CPU2MAX" ]]
-  then
+
+
+
+# Check if either CPU is over safety limit
+if [[ "$TEMPCPU1" > "$CPUMAXTEMP" || "$TEMPCPU2" > "$CPUMAXTEMP" ]]
+then
     ipmitool raw 0x30 0x30 0x01 0x01
-    echo "enable dynamic fan control"
-
-
-#22
-elif [[ "$TEMPINLET" > "$HIGHTEMP2"  ]]
-  then
+    echo "SAFETY: CPU temp exceeded $CPUMAXTEMP°C - reverted to auto fan control"
+    exit 1
+else
+    # Set static 40% fan speed
     ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS22"
-    echo "-->Set fan's to 22%"
-
-#20
-elif [[ "$TEMPINLET" > "$HIGHTEMP3" ||
-        "$TEMPCPU1" > "$CPU1MAX2" ||
-        "$TEMPCPU1" > "$CPU2MAX2"  ]]
-  then
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS20"
-    echo "--> Set fan's to 20%"
-
-
-#18
-elif [[ "$TEMPINLET" > "$HIGHTEMP4"  ]]
-  then
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS18"
-    echo "--> Set fan's to 18%"
-
-#16
-elif [[ "$TEMPINLET" > "$HIGHTEMP5"  ||
-        "$TEMPCPU1" > "$CPU1MAX3" ||
-        "$TEMPCPU1" > "$CPU2MAX3"  ]]
-  then
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS16"
-    echo "--> Set fan's to 16%"
-
-
-#14
-elif [[ "$TEMPINLET" > "$HIGHTEMP5"  ]]
-  then
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "FS14"
-    echo "--> Set fan's to 14%"
-
-#12
-elif [[ "$TEMPINLET" > "$HIGHTEMP5"  ]]
-  then
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS12"
-    echo "--> Set fan's to 12%"
-
-
-#anything less is 10
-  else
-    ipmitool raw 0x30 0x30 0x01 0x00
-    ipmitool raw 0x30 0x30 0x02 0xff "$FS10"
-    echo "--> Set fan's to 10%"
-
+    ipmitool raw 0x30 0x30 0x02 0xff "$STATICFAN"
+    echo "Static fan set to 40% - CPU1: $TEMPCPU1°C, CPU2: $TEMPCPU2°C"
 fi
+
+
